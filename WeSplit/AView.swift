9e1f319f80dcfef936911954d70bbb6c7145665c
David Shapiro <davids@dlsa.com>
2023-01-30 10:19:56 -0600

    First Commit

---
+++ WeSplit/AView.swift
@@ -0,0 +1,20 @@
+//
+//  AView.swift
+//  WeSplit
+//
+//  Created by David Shapiro on 1/30/23.
+//
+
+import SwiftUI
+
+struct AView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AView_Previews: PreviewProvider {
+    static var previews: some View {
+        AView()
+    }
+}
