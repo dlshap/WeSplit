75b94eb386cb36c9e07ad9fb7d95fa59bedf3615
David Shapiro <davids@dlsa.com>
2023-01-30 08:53:41 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by David Shapiro on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
